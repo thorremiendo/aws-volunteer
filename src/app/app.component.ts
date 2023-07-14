cfe4cb756f621573dba048a089b5cf5b78aecc39
Thor Remiendo <thorremiendo@gmail.com>
2023-07-14 23:09:26 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'aws-volunteers-page';
+export class AppComponent implements OnInit {
+  volunteers: any;
+  categoryKeys: string[] = [];
+  selectedCategory: string = 'All';
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.fetchVolunteers().subscribe((data: Category) => {
+      this.volunteers = data;
+      this.categoryKeys = Object.keys(this.volunteers);
+      console.log(this.volunteers, this.categoryKeys);
+    });
+  }
+
+  get allVolunteers(): Volunteer[] {
+    return this.categoryKeys.flatMap(category => this.volunteers[category]);
+  }
+
+  // fetchVolunteers(): Observable<Category> {
+  //   const url = 'https://z3efpmw3k2.execute-api.ap-southeast-1.amazonaws.com/dev/volunteers';
+  //   return this.http.get<Category>(url);
+  // }
+
+
+  fetchVolunteers(): Observable<Category> {
+    const url = 'assets/data/data.json';
+    return this.http.get<Category>(url);
+  }
+
+  getImagePath(imgPath: string) {
+    return `https://dl5qao1aeb4q4.cloudfront.net/${imgPath}`
+  }
+
+  selectCategory(category: string) {
+    this.selectedCategory = category;
+  }
+
+
+}
+
+
+interface Volunteer {
+  email_address: string;
+  complete_name: string;
+  image_path: string;
+  comittee: string;
+  role: string;
+}
+
+interface Category {
+  [key: string]: Volunteer[];
 }
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  purge: [],
+  darkMode: false, 
+  theme: {
+    extend: {
+      spacing: {
+        '120': '120px',
+      },
+      colors: {
+        'primary-blue': '#232F3E',
+        'secondary-blue': '#37475A',
+        'primary-orange': '#FF9E20',
+        'secondary-orange': '#FEBD69',
+        'secondary': '#FEBD69',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
